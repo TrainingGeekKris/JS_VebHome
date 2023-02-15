1714e0c8236d402f6edbb5637ba7a308e82fb359
Kristina <petrovnina22@mail.ru>
2023-02-15 18:18:51 +0300

    finish commit

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,25 @@
 // Задание №1.
 
-let nameUser=prompt(`Введите Ваше имя`);
+//let nameUser=prompt(`Введите Ваше имя`);
+//function messegeShow(){
+//    let messege=(`Привет, `+ nameUser);
+//    alert(messege);
+//    }
+//messegeShow();
+
+// Задание №2.
+
+let nameFilms=prompt(`Введите название любимого фильма`);
 function messegeShow(){
-    let messege=(`Привет, `+ nameUser);
-    alert(messege);
-    }
+if (nameFilms==Гнев) {
+    let messege=(`Верно, поздравляем!`);
+    alert (messege);
+    return;
+}
+else 
+{
+    let messege=(`Попробуй еще раз`);
+    alert (messege);
+}
+}
 messegeShow();
